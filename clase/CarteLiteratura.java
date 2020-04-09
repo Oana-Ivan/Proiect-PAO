d0bd9027aaf53a27b158d5e9d06b6d2c78719f42
Oana-Ivan <49486605+Oana-Ivan@users.noreply.github.com>
2020-04-09 23:13:25 +0300

    Update CarteLiteratura.java

--- clase/CarteLiteratura.java
+++ clase/CarteLiteratura.java
@@ -4,12 +4,27 @@ public class CarteLiteratura extends Carte{
     private String curentLiterar;
     private int nrVolume;
 
-    public CarteLiteratura(Autor autor, String titlu, int ISBN,  String curentLiterar, int nrVolume) {//int ziR, int lunaR, int anR,
-        super(autor, titlu, ISBN); //, ziR, lunaR, anR);
+    public CarteLiteratura() {
+        super();
+        this.curentLiterar = "";
+        this.nrVolume = 0;
+    }
+    public CarteLiteratura(String autor, String titlu, int ISBN,  String curentLiterar, int nrVolume) {
+        super(autor, titlu, ISBN);
         this.curentLiterar = curentLiterar;
         this.nrVolume = nrVolume;
     }
 
+    @Override
+    public void afisareDate() {
+        System.out.println("Titlu carte literatura: " + this.getTitlu());
+        System.out.println("Autor: " + this.getAutor());
+        System.out.println("ISBN: " + this.getISBN());
+        System.out.println("Curent literar: " + curentLiterar);
+        System.out.println("Nr volume: " + nrVolume);
+        System.out.println("Nr imprumuturi " + this.getNrInprumutari());
+    }
+    // metode get
     public String getCurentLiterar() {
         return curentLiterar;
     }
@@ -17,6 +32,7 @@ public class CarteLiteratura extends Carte{
         return nrVolume;
     }
 
+    // metode set
     public void setCurentLiterar(String curent) {
         curentLiterar = curent;
     }
