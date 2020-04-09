0c1d5ddea482d3e839711f8a1ddab5cfe5c141d1
Oana-Ivan <49486605+Oana-Ivan@users.noreply.github.com>
2020-04-09 23:20:48 +0300

    Update Main.java

--- clase/Main.java
+++ clase/Main.java
@@ -1,15 +1,150 @@
 package clase;
 
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         // afisare meniu
+        afisareMeniu();
+
+        Scanner input = new Scanner(System.in);
+        Gestiune g = new Gestiune();
+        int optiuneAleasa = 1;
+
+        while (optiuneAleasa != 0) {
+            System.out.println("Selectati optiune din meniu");
+            optiuneAleasa = input.nextInt();
+            String enter = input.nextLine();
+            switch (optiuneAleasa) {
+                case 0:
+                    break;
+                case 1: {
+                    // citire date de la tastatura
+                    System.out.println("Titlul cartii: "); String titlu = input.nextLine();
+                    System.out.println("Numele autorului:"); String numeAutorCarte = input.nextLine();
+                    System.out.println("ISBN: "); int ISBN = input.nextInt();
+                    enter = input.nextLine();
+                    System.out.println("Curent literar: "); String curentLiterar = input.nextLine();
+                    System.out.println("Nrmar de volume: "); int nrVolume = input.nextInt();
+                    enter = input.nextLine();
+
+                    // apelare functie din gestiune
+                    g.adaugareCarteLiteratura(numeAutorCarte, titlu, ISBN, curentLiterar, nrVolume);
+                    break;
+                }
+                case 2: {
+                    // citire date de la tastatura
+                    System.out.println("Titlul cartii: "); String titluCarteS = input.nextLine();
+                    System.out.println("Numele autorului:"); String numeAutorCarteS = input.nextLine();
+                    System.out.println("ISBN: "); int ISBNS = input.nextInt();
+                    enter = input.nextLine();
+                    System.out.println("Numar de termeni din index: "); int nrTermeniIndex = input.nextInt();
+                    enter = input.nextLine();
+                    System.out.println("Numar de surse bibliografice: "); int nrSurseBibliografice = input.nextInt();
+                    enter = input.nextLine();
+
+                    // apelare functie din gestiune
+                    g.adaugareCarteStiintifica(numeAutorCarteS, titluCarteS, ISBNS, nrTermeniIndex, nrSurseBibliografice);
+                    break;
+                }
+                case 3: {
+                    g.afisareCarti();
+                    break;
+                }
+                case 4: {
+                    g.afisareCeaMaiCititaCarte();
+                    break;
+                }
+                case 5: {
+                    // citire date de la tastatura
+                    System.out.println("Numele bibliotecar:"); String numeBibliotecar = input.nextLine();
+                    System.out.println("Varsta: "); int varsta = input.nextInt();
+                    System.out.println("Salariu: "); int salariu = input.nextInt();
+                    System.out.println("Vechime: "); int vechime = input.nextInt();
+
+                    // apelare functie din gestiune
+                    g.adaugareBibliotecar(numeBibliotecar, varsta, salariu, vechime);
+                    break;
+                }
+                case 6: {
+                    g.afisareBibliotecari();
+                    break;
+                }
+                case 7: {
+                    // citire date de la tastatura
+                    System.out.println("Nume autor: "); String numeAutor = input.nextLine();
+                    System.out.println("Varsta autorului:"); int varstaAutor = input.nextInt();
+                    enter = input.nextLine();
+                    System.out.println("Numele unei carti scrise de autor"); String numeCarte = input.nextLine();
+                    System.out.println("ISBN:"); int isbn = input.nextInt();
+                    enter = input.nextLine();
+
+                    // apel functie din gestiune
+                    g.adaugareAutor(numeAutor, varstaAutor, numeCarte, isbn);
+                    break;
+                }
+                case 8: {
+                    g.afisareAutori();
+                    break;
+                }
+                case 9: {
+                    // citire date de la tastatura
+                    System.out.println("Numele abonat:"); String nume = input.nextLine();
+                    System.out.println("Varsta: "); int varstaAbonat = input.nextInt();
+                    enter = input.nextLine();
+                    System.out.println("Nume abonament: "); String numeAbonament = input.nextLine();
+                    System.out.println("Penalizari anterioare: "); int penalizari = input.nextInt();
+                    enter = input.nextLine();
+
+                    // apel functie din gestiune
+                    g.adaugareAbonat(nume, varstaAbonat, numeAbonament, penalizari);
+                    break;
+                }
+                case 10: {
+                    g.afisareAbonati();
+                    break;
+                }
+                case 11: {
+                    // citire date de la tastatura
+                    System.out.println("Denumire abonament: "); String denumire = input.nextLine();
+                    System.out.println("Pret: "); int pret = input.nextInt();
+                    enter = input.nextLine();
+                    System.out.println("Limita de carti ce pot fi imprumutate: "); int limitaNrCarti = input.nextInt();
+                    enter = input.nextLine();
+
+                    // apel functie
+                    g.adaugareAbonament(denumire, pret, limitaNrCarti);
+                    break;
+                }
+                case 12: {
+                    g.afisareAbonamente();
+                    break;
+                }
+                case 13: {
+                    System.out.println("Numele abonatului"); String numeAbonat = input.nextLine();
+                    System.out.println("Titlul cartii: "); String numeCarte = input.nextLine();
+
+                    g.adaugareImprumut(numeAbonat, numeCarte);
+                    break;
+                }
+                case 14: {
+                    g.sortareCartiNrImprumuturi();
+                    break;
+                }
+                default:
+                    System.out.println("Optiunea aleasa nu exista.");
+
+            }
+        }
+    }
+
+    public static void afisareMeniu() {
         System.out.println("Alegeti intre:");
         System.out.println("0. Iesire");
-        System.out.println("1. Adaugare carte");
-        System.out.println("2. Afisare carti");
-        System.out.println("3. Stergere carte");
+        System.out.println("1. Adaugare carte literatura");
+        System.out.println("2. Adaugare carte stiintifica");
+        System.out.println("3. Afisare carti");
         System.out.println("4. Cea mai citita carte");
         System.out.println("5. Adaugare bibliotecar");
         System.out.println("6. Afisare Bibliotecari");
@@ -19,66 +154,7 @@ public class Main {
         System.out.println("10. Afisare abonati");
         System.out.println("11. Adaugare abonament");
         System.out.println("12. Afisare abonamente");
-
-        Scanner input = new Scanner(System.in);
-        Gestiune g = new Gestiune();
-        int p = 1;
-        while (p != 0) {
-            p = input.nextInt();
-            String enter = input.nextLine();
-            switch (p) {
-                case 0:
-                    break;
-                case 1: // adaugare carte
-                    System.out.println("Numele autorului:");
-                    String numeAutor = input.nextLine();
-                    System.out.println("Titlul cartii: ");
-                    String titlu = input.nextLine();
-                    System.out.println("ISBN: ");
-                    int ISBN = input.nextInt();
-
-                    g.adaugareCarte(numeAutor, titlu, ISBN);
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;
-                case 6:
-                    break;
-                case 7:
-                    break;
-                case 8:
-                    break;
-                case 9:
-                    break;
-                case 10:
-                    break;
-                case 11:
-                    break;
-                case 12:
-                    break;
-                default:
-
-            }
-        }
-//        Scanner input = new Scanner(System.in);
-////        String userName = input.nextLine();
-//        Gestiune g = new Gestiune();
-//        Bibliotecar b = new Bibliotecar("Andrei", 32, 3000, 5);
-//        String nume = input.nextLine();
-//        int v = input.nextInt();
-//        int s = input.nextInt();
-//        int vech = input.nextInt();
-//        Bibliotecar z = new Bibliotecar(nume, v, s, vech);
-//        g.adaugareBibliotecar(z);
-//
-////        b.afisareDateComplete();
-//        g.adaugareBibliotecar(b);
-//        g.afisareBibliotecari();
-
+        System.out.println("13. Adauga imprumut");
+        System.out.println("14. Sortare carti dupa numar imprumuturi");
     }
 }
